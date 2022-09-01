cceaa340b127f5f12e31e9223d2337bca6e5b557
corecaps <corecaps@gmail.com>
2022-09-01 18:05:17 +0200

    Parser boilerplate

--- parser.c
+++ parser.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jgarcia <jgarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/01 11:09:22 by jgarcia           #+#    #+#             */
-/*   Updated: 2022/09/01 11:12:44 by jgarcia          ###   ########.fr       */
+/*   Created: 2022/09/01 17:57:11 by jgarcia           #+#    #+#             */
+/*   Updated: 2022/09/01 17:57:39 by jgarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-void	ft_error(int code)
-{
-	// TODO print error code
-	exit(code);
-}
 
-int	main(int argc, char **argv)
+int	parser(int argc,char **argv,t_node *top_a,t_node *top_b)
 {
-	t_node *top_a;
-	t_node *top_b;
 
-	top_a = NULL;
-	top_b = NULL;
-	if (parser(argc, argv, top_a, top_b))
-		ft_error(1);
-	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -18,4 +18,8 @@ typedef struct s_node
 	int n;
 	struct s_node *next;
 }t_node;
+int		parser(int argc,char **argv,t_node *top_a,t_node *top_b);
+t_node	*push(t_node *top, int n);
+t_node	*get_bottom(t_node *top);
+int		pop(t_node **top);
 #endif
