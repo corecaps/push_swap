fb2e9c944384e167abfb11e780478718f9235f06
corecaps <corecaps@gmail.com>
2022-11-19 16:45:59 +0100

    improved tests

--- tests.py
+++ tests.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+import subprocess
 import sys
 import time
 
@@ -10,17 +10,20 @@ INT_OVERFLOW_MAX = 82147483647
 
 
 def main():
-    n = sys.argv[1]
-    arg_array = np.random.randint(INT_MIN, INT_MAX, int(n))
-    args = ""
-    for i in arg_array:
-        args += str(i)
-        args += " "
-    start = time.perf_counter()
-    os.system('./push_swap ' + args)
-    stop = time.perf_counter()
-    print(f"Elapsed time : {stop - start:0.4f} sec\n")
-
+    x = [3,5,10,100,500]
+    #n = sys.argv[1]
+    for n in x:
+        T = int(sys.argv[1])
+        result= []
+        for j in range(T):
+            arg_array = np.random.randint(INT_MIN, INT_MAX, int(n))
+            args = ""
+            for i in arg_array:
+                args += str(i)
+                args += " "
+            r = int(subprocess.check_output('./push_swap ' + args + '| wc -l', shell=True))
+            result.append(r)
+        print(f"Sorting {n} numbers over {T} iterations\nCounting operations : \tAverage:\t{int(sum(result)/len(result))}\tMax:\t{max(result)}\tMin:\t{min(result)}\n")
 
 if __name__ == "__main__":
     main()
